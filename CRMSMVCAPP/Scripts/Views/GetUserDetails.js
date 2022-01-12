cb66145457bb06350e8134777d72db429d1285e8
Shubham Sharma <Shubhamsharma@live.com>
2022-01-12 15:40:46 +0530

    Created datatable for user details.

---
+++ CRMSMVCAPP/Scripts/Views/GetUserDetails.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    var table = $('#example').DataTable({
+
+        lengthChange: false,
+        buttons: ['copy', 'excel', 'pdf', 'colvis']
+    });
+
+    table.buttons().container()
+        .insertBefore('#example_filter');
+});
