c1d488e81e46656ebb76d83c5db95a61497856b3
Shubham Sharma <Shubhamsharma@live.com>
2022-06-22 18:35:43 +0530

    Notification dispaly changes.

--- CRMSMVCAPP/Scripts/Views/ControlsScript.js
+++ CRMSMVCAPP/Scripts/Views/ControlsScript.js
@@ -51,3 +51,36 @@ var ControlScript = ControlScript ||
 
 
 };
+
+//Changes for Notification display
+
+DisplayNotificationBar = function () {
+
+    ShowMessage("Internal exception Occured.", 'Error');
+
+}
+
+function ShowMessage(message, messagetype) {
+    var cssclass;
+    switch (messagetype) {
+        case 'Success':
+            cssclass = 'alert-success'
+            break;
+        case 'Error':
+            cssclass = 'alert-danger'
+            break;
+        case 'Warning':
+            cssclass = 'alert-warning'
+            break;
+        default:
+            cssclass = 'alert-info'
+    }
+
+    $('#alert_container').append('<div id="alert_div" style="margin: 0 0.5%; -webkit-box-shadow: 3px 4px 6px #999;" class="alert fade in ' + cssclass + '"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>' + messagetype + '!</strong> <span>' + message + '</span></div>');
+
+    setTimeout(function () {
+        $("#alert_div").fadeTo(2000, 500).slideUp(500, function () {
+            $("#alert_div").remove();
+        });
+    }, 5000);//5000=5 seconds
+}
